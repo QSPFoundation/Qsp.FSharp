4fbbfc048886ba43e534c6538fbb07ecfdeb218a
gretmn102 <erlgreywatson@gmail.com>
2024-04-25 13:38:44 +0300

    build(core): make `Qsp.FSharp.Core` as library

--- build.fsx
+++ build.fsx
@@ -18,7 +18,8 @@ let testProjPath = "tests" </> "Qsp.FSharp.Core.Tests" </> sprintf "%s.fsproj" t
 let serverProjName = "Qsp.FSharp.LanguageServer"
 let serverProjPath = "src" </> "Qsp.FSharp.LanguageServer" </> sprintf "%s.fsproj" serverProjName
 let parserProjName = "Qsp.FSharp.Core"
-let parserProjPath = "src" </> "Qsp.FSharp.Core" </> sprintf "%s.fsproj" parserProjName
+let parserProjDir = "src" </> "Qsp.FSharp.Core"
+let parserProjPath = parserProjDir </> sprintf "%s.fsproj" parserProjName
 let utilityProjName = "Utility"
 let utilityProjpath = "Utility" </> sprintf "%s.fsproj" utilityProjName
 // --------------------------------------------------------------------------------------
@@ -30,6 +31,13 @@ let dotnet cmd workingDir =
     let result = DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
     if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
+module XmlText =
+    let escape rawText =
+        let doc = new System.Xml.XmlDocument()
+        let node = doc.CreateElement("root")
+        node.InnerText <- rawText
+        node.InnerXml
+
 let targetFrameworks = ["net461"; "netcoreapp3.1"]
 // --------------------------------------------------------------------------------------
 // Targets
@@ -37,17 +45,66 @@ let targetFrameworks = ["net461"; "netcoreapp3.1"]
 
 let commonBuildArgs =
     [
-        yield "-c Release"
-        if not Environment.isWindows then
-            yield "--framework netcoreapp3.1"
+        "-c Release"
     ]
     |> String.concat " "
 
 let dotnetBuild =
+    let commonBuildArgs =
+        [
+            yield commonBuildArgs
+            if not Environment.isWindows then
+                yield "--framework netcoreapp3.1"
+        ]
+        |> String.concat " "
     dotnet (sprintf "build %s" commonBuildArgs)
 
 let dotnetRun framework =
     dotnet (sprintf "run --framework %s" framework)
+
+Target.create "CoreMeta" (fun _ ->
+    let release = ReleaseNotes.load (parserProjDir </> "RELEASE_NOTES.md")
+
+    [
+        "<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">"
+        "<ItemGroup>"
+        "    <PackageReference Include=\"Microsoft.SourceLink.GitHub\" Version=\"1.0.0\" PrivateAssets=\"All\"/>"
+        "</ItemGroup>"
+        "<PropertyGroup>"
+        "    <EmbedUntrackedSources>true</EmbedUntrackedSources>"
+        "    <PackageProjectUrl>https://github.com/gretmn102/FParserQSP/tree/master/src/Qsp.FSharp.Core</PackageProjectUrl>"
+        "    <PackageLicenseExpression>MIT</PackageLicenseExpression>"
+        "    <RepositoryUrl>https://github.com/gretmn102/FParserQSP.git</RepositoryUrl>"
+        sprintf "    <PackageReleaseNotes>%s</PackageReleaseNotes>"
+            (String.concat "\n" release.Notes |> XmlText.escape)
+        "    <PackageTags>fsharp;qsp;parser</PackageTags>"
+        "    <Authors>gretmn102</Authors>"
+        sprintf "    <Version>%s</Version>" release.SemVer.AsString
+        "</PropertyGroup>"
+        "</Project>"
+    ]
+    |> File.write false (parserProjDir </> "Directory.Build.props")
+)
+
+Target.create "CorePack" (fun _ ->
+    let commonBuildArgs =
+        [
+            yield commonBuildArgs
+            if not Environment.isWindows then
+                yield "-p:TargetFrameworks=netstandard2.0"
+        ]
+        |> String.concat " "
+    dotnet (sprintf "pack %s" commonBuildArgs) parserProjDir
+)
+
+Target.create "CorePushToGitlab" (fun _ ->
+    let release = ReleaseNotes.load (parserProjDir </> "RELEASE_NOTES.md")
+    let deployDir = parserProjDir </> "bin" </> "Release"
+    let packPath =
+        sprintf "Qsp.FSharp.Core.%s.nupkg" release.SemVer.AsString
+    deployDir
+    |> dotnet (sprintf "nuget push -s %s %s" "gitlab" packPath)
+)
 
 Target.create "BuildServer" (fun _ ->
     serverProjPath
@@ -96,6 +153,10 @@ Target.create "TrimTrailingWhitespace" (fun _ ->
 open Fake.Core.TargetOperators
 
 Target.create "Default" ignore
+
+"CoreMeta"
+  ==> "CorePack"
+  ==> "CorePushToGitlab"
 
 "BuildServer"
   ==> "Default"
