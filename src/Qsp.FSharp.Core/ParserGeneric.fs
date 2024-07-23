0230875624044593aeda44832327e03e120dad5a
gretmn102 <erlgreywatson@gmail.com>
2024-07-23 11:49:41 +0300

    refactor(core): remove `DocumentHighlightKind`

--- src/Qsp.FSharp.Core/ParserGeneric.fs
+++ src/Qsp.FSharp.Core/ParserGeneric.fs
@@ -56,17 +56,6 @@ let notFollowedVarCont<'UserState> =
     notFollowedBy (satisfy isIdentifierChar)
     : Parser<_, 'UserState>
 
-/// A document highlight kind.
-[<RequireQualifiedAccess>]
-type DocumentHighlightKind =
-    /// A textual occurrence.
-    | Text = 1
-
-    /// Read-access of a symbol, like reading a variable.
-    | Read = 2
-
-    /// Write-access of a symbol, like writing to a variable.
-    | Write = 3
 type VarHighlightKind =
     | ReadAccess
     | WriteAccess
