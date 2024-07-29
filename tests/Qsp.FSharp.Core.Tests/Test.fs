cbd2570efcc214f7444dad8faf7b15b3a7076940
gretmn102 <erlgreywatson@gmail.com>
2024-07-29 14:25:30 +0300

    refactor(test): move `pbracesTests` to `Parser/Generic.Tests.fs`

--- tests/Qsp.FSharp.Core.Tests/Test.fs
+++ tests/Qsp.FSharp.Core.Tests/Test.fs
@@ -161,40 +161,6 @@ let stringLiteralWithTokenTest =
                 [[HyperLinkKind (Raw " '<<'x'>>'", [[StringKind "action"]])]]
             Assert.Equal("", Right exp, runEither input)
     ]
-[<Tests>]
-let pbracesTests =
-    let runEither str =
-        Qsp.Parser.Generic.runStateEither
-            (pbraces Tokens.TokenType.StringBraced)
-            Qsp.Parser.Generic.State.empty
-            str
-        |> snd
-    testList "stringLiteralWithTokenTest" [
-        testCase "base" <| fun () ->
-            Assert.Equal("", Right "", runEither "{}")
-        testCase "braces1" <| fun () ->
-            Assert.Equal("", Right "abc", runEither "{abc}")
-        testCase "1" <| fun () ->
-            let input =
-                [
-                    "{"
-                    "    asdf"
-                    "    {"
-                    "        asdf"
-                    "    }"
-                    "}"
-                ] |> String.concat "\n"
-            let exp =
-                [
-                    ""
-                    "    asdf"
-                    "    {"
-                    "        asdf"
-                    "    }"
-                    ""
-                ] |> String.concat "\n"
-            Assert.Equal("", Right exp, runEither input)
-    ]
 
 let printStmts stmts =
     List.collect (Statement.Printer.showStmt (IndentsOption.UsingSpaces 4) Printer.FormatConfig.Default) stmts
