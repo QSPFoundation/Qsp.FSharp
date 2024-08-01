8e983f314f15c5ec8e40f9d41577bf448a41f1af
gretmn102 <erlgreywatson@gmail.com>
2024-08-01 21:49:18 +0300

    refactor(test): improve `Test.fs`

--- tests/Qsp.FSharp.Core.Tests/Test.fs
+++ tests/Qsp.FSharp.Core.Tests/Test.fs
@@ -1,28 +1,8 @@
-open FsharpMyExtension
-open FsharpMyExtension
-open FsharpMyExtension.Either
-open FParsec
-#if INTERACTIVE
-#load @"..\QSParse\Tokens.fs"
-#load @"..\QSParse\Ast.fs"
-#load @"..\QSParse\Defines.fs"
-#load @"..\QSParse\Show.fs"
-#load @"..\QSParse\ParserGeneric.fs"
-#load @"..\QSParse\ParserExpr.fs"
-#load @"..\QSParse\Parsec.fs"
-#endif
-open Qsp
-open Qsp.Ast
-open Qsp.Printer
-open Qsp.Printer.Ast
-open Qsp.Parser.Generic
-open Qsp.Parser.Ast
-
+module Qsp.Tests.Test
 open Fuchu
 
+open Qsp
 open Tests
-
-// #load "Parsec.fs"
 
 [<EntryPoint;System.STAThread>]
 let main args =
@@ -36,15 +16,20 @@ let main args =
                 whileYOrN ()
         printfn "Full test? (`y` or `n`)"
         whileYOrN ()
+
     let f isFullTest =
         if isFullTest then
             defaultMainThisAssembly args
         else
-            defaultMainThisAssemblyFilter args
+            defaultMainThisAssemblyFilter
+                args
                 (fun x ->
-                    x.Where(fun x -> not <| x.StartsWith TestOnMocks.mockTestList))
+                    x.Where(fun x -> not <| x.StartsWith TestOnMocks.mockTestList)
+                )
+
     match args with
-    | [|"--full"|] -> f true
+    | [|"--full"|] ->
+        f true
     | [||] ->
         f (isFullTest ())
     | _ ->
